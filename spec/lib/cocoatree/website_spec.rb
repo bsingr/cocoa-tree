b6f92df1c3f926d84ff2435828e0847a006d704f
Jens Bissinger <mail@jens-bissinger.de>
2013-07-10 21:04:59 +0200

    Implement basic pods list html. Refs #12.

---
+++ spec/lib/cocoatree/website_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'cocoatree'
+
+describe Cocoatree::Website do
+  let(:website) { described_class.new }
+  let(:pods) { Cocoatree::Pods.new }
+  before do
+    pods.source_path = File.join(Cocoatree.root, 'Specs')
+    Octokit.stub(:stargazers) do
+      Array.new(rand(100))
+    end
+  end
+
+  it do
+    website.pods = pods
+    rendered = website.render('list.html')
+    rendered.should include('<html>')
+    rendered.should include('<li>')
+  end
+end
