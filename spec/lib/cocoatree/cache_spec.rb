32b66a6a58eb3593d185df7e41a28e9a4ba217c8
Jens Bissinger <mail@jens-bissinger.de>
2013-07-25 21:56:21 +0200

    Fix cache spec

--- spec/lib/cocoatree/cache_spec.rb
+++ spec/lib/cocoatree/cache_spec.rb
@@ -3,10 +3,10 @@ require 'cocoatree/cache'
 
 describe Cocoatree::Cache do
   before do
-    Cocoatree.stub(:root).and_return(assets_path('example_cache'))
+    Cocoatree.stub(:root).and_return(assets_path)
   end
 
-  subject('cache') { described_class.new 'rspec-cache' }
+  subject('cache') { described_class.new 'example_cache' }
 
   its(:path) { should include('example_cache') }
 end
