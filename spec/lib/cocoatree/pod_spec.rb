948d56e306547138403366d328f30b015d6c3063
Jens Bissinger <mail@jens-bissinger.de>
2013-07-27 23:57:42 +0200

    Add basic pod spec

---
+++ spec/lib/cocoatree/pod_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'cocoatree/pod'
+
+describe Cocoatree::Pod do
+  let('spec') do
+    double('Spec').tap do |s|
+      s.stub('source').and_return git: 'git-url'
+    end
+  end
+
+  subject('pod') { described_class.new spec }
+
+  its('url') { should == 'git-url' }
+end
