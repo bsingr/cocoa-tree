b2e3ddab99a3212f851faa5b9b07fe6391f49e78
Jens Bissinger <mail@jens-bissinger.de>
2013-07-28 00:13:12 +0200

    Add pod specs

--- spec/lib/cocoatree/pod_spec.rb
+++ spec/lib/cocoatree/pod_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'cocoatree/pod'
+require 'octokit'
 
 describe Cocoatree::Pod do
   let('spec') do
@@ -24,5 +25,30 @@ describe Cocoatree::Pod do
     its('url') { should == 'https://github.com/foo/bar.git' }
     its('github') { should == 'foo/bar' }
     its('github') { should be_true}
+  end
+
+  describe 'stars' do
+    let('github_cache') do
+      double('GithubCache').tap do |c|
+        c.stub('fetch') do |id, key, &block|
+          1337
+        end
+      end
+    end
+    before do
+      Cocoatree.stub('github_cache').and_return github_cache
+    end
+
+    its('stars') { should == 1337 }
+
+    context 'not cached' do
+      before do
+        github_cache.stub('fetch') do |id, key, &block|
+          block.call
+        end
+      end
+
+      its('stars') { should == -1 }
+    end
   end
 end
