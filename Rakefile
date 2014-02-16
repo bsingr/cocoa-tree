2565d6d3c6cfd66361ace3cc8305133009cc9195
Jens Bissinger <mail@jens-bissinger.de>
2014-02-16 20:02:00 +0100

    Transform into rails 4 app.

--- Rakefile
+++ Rakefile
@@ -1,10 +1,6 @@
-require "bundler/gem_tasks"
-require 'yaml'
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
-lib = File.expand_path('lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'cocoatree'
+require File.expand_path('../config/application', __FILE__)
 
-load 'lib/tasks/site.rake'
-
-task :default => :spec
+Cocoatree::Application.load_tasks
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cocoatree::Application.config.secret_key_base = 'edcf993e1b1d8daa6e1a7ccfbeaec16ac5c18fbcae5c326f8aaf03f2f47deba2f246f3a0475eff6c20eb1f531e46aa0abf1dcbb5149ff9c0ef82ed3d9debf5e1'
