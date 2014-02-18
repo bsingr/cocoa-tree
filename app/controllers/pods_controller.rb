4183a1e2054fdeb2bd898aa1aeff5397fefa7dd1
Jens Bissinger <mail@jens-bissinger.de>
2014-02-18 08:50:55 +0100

    Faster pods#index.

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -1,9 +1,11 @@
-require 'cocoatree'
-
 class PodsController < ApplicationController
   caches_page :index
   
   def index
-    @pods = Cocoatree.pods.pods
+    pods = Cocoatree.pods
+    @pods = pods.source.pods[0..50]\
+      .map { |p| pods.pod(p) }\
+      .compact\
+      .keep_if(&:github?)
   end
 end
