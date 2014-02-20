2957cc9f80ee0c70b2d8b6bec666a7f259ec3c43
Jens Bissinger <mail@jens-bissinger.de>
2014-02-20 23:19:24 +0100

    Optionally use config/github.yml for octokit.

---
+++ config/initializers/octokit.rb
@@ -0,0 +1,10 @@
+github_conf_file = Rails.root.join('config', 'github.yml')
+if File.exists?(github_conf_file)
+  puts "Detected #{github_conf_file}"
+  github_config = YAML.load_file(github_conf_file)
+  puts "Using github login = #{github_config['login']}"
+  Octokit.configure do |c|
+    c.login = github_config['login']
+    c.access_token = github_config['oauth_token']
+  end
+end
