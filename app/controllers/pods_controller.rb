a68d8c142b42b5fd26b157cde6baa9c0f07992bc
Jens Bissinger <mail@jens-bissinger.de>
2014-02-20 21:42:46 +0100

    Replace website-src by pods#index.

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -1,14 +1,8 @@
-require 'cocoatree'
-
 class PodsController < ApplicationController
   caches_page :index
   
   def index
-    pods = Cocoatree.pods
-    @pods = pods.source.pods[0..50]\
-      .map { |p| pods.pod(p) }\
-      .compact\
-      .keep_if(&:github?)
+    @pods = CocoaPod.all
     @pods.keep_if{|p| p.name =~ /#{params[:filter]}/}
   end
 end
