a07503724da6078f289bb1d56114575bd1a3e93a
Jens Bissinger <mail@jens-bissinger.de>
2014-02-20 22:59:39 +0100

    Use batch find in job.

--- app/jobs/update_cocoa_pod_from_github_job.rb
+++ app/jobs/update_cocoa_pod_from_github_job.rb
@@ -1,7 +1,7 @@
 class UpdateCocoaPodFromGithubJob
   def run
     github_updater = GithubUpdater.new
-    CocoaPod.all.each do |cocoa_pod|
+    CocoaPod.all.find_each do |cocoa_pod|
       puts "sync github #{cocoa_pod.name}"
       github_updater.update cocoa_pod
     end
