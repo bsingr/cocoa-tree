b14a6c378cdaa92b961e87e342e162dbffa29573
Jens Bissinger <mail@jens-bissinger.de>
2014-02-20 18:56:48 +0100

    Add github updater.

--- app/updaters/github_updater.rb
+++ app/updaters/github_updater.rb
@@ -2,10 +2,20 @@ class GithubUpdater
   def update cocoa_pod
     if repository = GithubUrl.new.shortcut(cocoa_pod.source_url)
       if data = get_github_repository(repository)
-         cocoa_pod.pushed_at = data['pushed_at']
-         cocoa_pod.stars = data['watchers_count']
+         cocoa_pod.pushed_at = normalize_time(data['pushed_at'])
+         cocoa_pod.stars = data['watchers_count'] || 0
          cocoa_pod.save!
       end
+    end
+  end
+  
+  def normalize_time time
+    if time.kind_of? Time
+      time
+    elsif time.kind_of? String
+      Time.parse(time)
+    else
+      nil
     end
   end
   
