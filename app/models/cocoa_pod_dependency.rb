146b6df9596c30f9d6b4bceb60e4b013bc650e3c
Jens Bissinger <mail@jens-bissinger.de>
2014-02-21 22:24:27 +0100

    Fix cocoa pod dependency model relationships.

--- app/models/cocoa_pod_dependency.rb
+++ app/models/cocoa_pod_dependency.rb
@@ -1,4 +1,4 @@
 class CocoaPodDependency < ActiveRecord::Base
-  belongs_to :cocoa_pod_id
-  belongs_to :dependent_cocoa_pod_id
+  belongs_to :cocoa_pod
+  belongs_to :dependent_cocoa_pod, class_name: 'CocoaPod'
 end
