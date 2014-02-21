6b8f46c2d02debd02e8f59febf23f24bde8016c1
Jens Bissinger <mail@jens-bissinger.de>
2014-02-21 20:28:35 +0100

    Remove deprecated stuff.

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -5,7 +5,6 @@ class PodsController < ApplicationController
     @pods = CocoaPod.all
     @pods.keep_if{|p| p.name =~ /#{params[:filter]}/}
     respond_to do |format|
-      #format.mpac { render :text => MessagePack.dump(@pods[0].map{|p| p.serializable_hash}) }
       format.mpac { render :text => MessagePack.dump(@pods.map(&:serializable_hash)) }
       format.json { render json: @pods }
       format.html
