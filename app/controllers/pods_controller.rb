25cd4c82d878f07102435ac96cc3ea51a5137cb3
Jens Bissinger <mail@jens-bissinger.de>
2014-02-22 12:04:58 +0100

    Cleanup cocoa pods controller.

--- app/controllers/pods_controller.rb
+++ app/controllers/pods_controller.rb
@@ -1,16 +1,18 @@
 class PodsController < ApplicationController
   caches_page :index, :show
   
+  CHUNK_SIZE = 100
+  
   def index
     @pods = CocoaPod.all
-    pods_index = CocoaPod.all.each_slice(100).each_with_index.map do |pods, index|
+    pods_index = CocoaPod.all.each_slice(CHUNK_SIZE).each_with_index.map do |pods, index|
       [index, pods.map(&:name)]
     end
     gon.push(pods_index: pods_index)
   end
 
   def show
-    data = CocoaPod.all.each_slice(100).to_a
+    data = CocoaPod.all.each_slice(CHUNK_SIZE).to_a
     @pods = data[params[:id].to_i]
     respond_to do |format|
       format.mpac { render :text => MessagePack.dump(@pods.map(&:serializable_hash)) }
