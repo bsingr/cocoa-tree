8c6d75bfb29ae1c08df729cdb1d2992d3c9ae47f
Jens Bissinger <mail@jens-bissinger.de>
2014-02-23 15:23:04 +0100

    Use rails logger instead of puts.

--- app/jobs/update_cocoa_pod_from_github_job.rb
+++ app/jobs/update_cocoa_pod_from_github_job.rb
@@ -3,7 +3,7 @@ class UpdateCocoaPodFromGithubJob
     github_updater = GithubUpdater.new
     CocoaPod.all.find_each do |cocoa_pod|
       if Time.now > (cocoa_pod.updated_at + 1.day) || !cocoa_pod.pushed_at
-        puts "sync github #{cocoa_pod.name}"
+        Rails.logger.info "sync github #{cocoa_pod.name}"
         github_updater.update cocoa_pod
       end
     end
