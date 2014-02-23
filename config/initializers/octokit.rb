a7e5afceee63685ef8de193a9b7f717ff1ac5629
Jens Bissinger <mail@jens-bissinger.de>
2014-02-23 15:30:22 +0100

    Allow to pass octokit credentials via environment variables.

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -7,4 +7,11 @@ if File.exists?(github_conf_file)
     c.login = github_config['login']
     c.access_token = github_config['access_token']
   end
+elsif ENV.has_key?('GITHUB_LOGIN') && ENV.has_key?('GITHUB_TOKEN')
+  puts "Detected GITHUB_LOGIN and GITHUB_TOKEN"
+  puts "Using github login = #{ENV['GITHUB_LOGIN']}"
+  Octokit.configure do |c|
+    c.login = ENV['GITHUB_LOGIN']
+    c.access_token = ENV['GITHUB_TOKEN']
+  end
 end
