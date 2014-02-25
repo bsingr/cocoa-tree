2abfd0be325e616bf0f99e8f349440eedd38e10d
Jens Bissinger <mail@jens-bissinger.de>
2014-02-25 19:10:25 +0100

    Add cocoa pods dependents.

--- app/models/cocoa_pod.rb
+++ app/models/cocoa_pod.rb
@@ -2,6 +2,8 @@ class CocoaPod < ActiveRecord::Base
   default_scope { order('stars DESC') }
 
   has_many :cocoa_pod_dependencies, foreign_key: :dependent_cocoa_pod_id
+  has_many :dependent_cocoa_pods, foreign_key: :cocoa_pod_id,
+                                  class_name: 'CocoaPodDependency'
   has_and_belongs_to_many :dependencies,
                           class_name: 'CocoaPod',
                           join_table: 'cocoa_pod_dependencies',
@@ -28,7 +30,10 @@ class CocoaPod < ActiveRecord::Base
       'summary' => summary,
       'dependencies' => cocoa_pod_dependencies.map{|d| {'id' => d.cocoa_pod.id,
                                                         'name' => d.cocoa_pod.name,
-                                                        'requirement' => d.requirement }}
+                                                        'requirement' => d.requirement }},
+      'dependents' => dependent_cocoa_pods.map{|d| {'id' => d.dependent_cocoa_pod.id,
+                                                    'name' => d.dependent_cocoa_pod.name,
+                                                    'requirement' => d.requirement }}
     }
   end
 end
