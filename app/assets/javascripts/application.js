d72fbb29ecfc0924a3f3c1716356285cc371e743
Jens Bissinger <mail@jens-bissinger.de>
2014-03-04 07:36:29 +0100

    Add json2.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require lib/json2.js
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
