078137bd9baadfffa348685feabe51a076ce4a99
Jens Bissinger <mail@jens-bissinger.de>
2014-04-03 21:18:55 +0200

    Fix rails zero config for heroku.

--- config/initializers/rails_zero.rb
+++ config/initializers/rails_zero.rb
@@ -1,7 +1,8 @@
 require 'rails_zero/drivers/poltergeist'
 
 RailsZero.configure do |c|
+  c.backend.url = 'http://cocoa-tree.herokuapp.com' if Rails.env.production?
   c.site.add_path '/'
   c.deployment.url = 'git@github.com:cocoa-tree/cocoa-tree.github.io.git'
-  c.deployment.git_binary = 'git'
+  c.deployment.git_binary = 'git' unless Rails.env.production?
 end
