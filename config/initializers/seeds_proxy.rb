494d4ed8cd43c3f8806b006403e1053ec4e2273c
Jens Bissinger <mail@jens-bissinger.de>
2014-04-11 08:24:17 +0200

    Configure seeds proxy via ENV['SEEDS'].

--- config/initializers/seeds_proxy.rb
+++ config/initializers/seeds_proxy.rb
@@ -1,10 +1,16 @@
 class SeedsProxy < Rack::Proxy
-  USE = :remote
+  USE = ENV['SEEDS'] || 'remote'
   LOCAL_URL = 'localhost:3001'
   REMOTE_URL = 'cocoa-tree.github.io:80'
 
+  if USE == 'local'
+    puts "=> Using seeds from #{LOCAL_URL}"
+  else
+    puts "=> Using seeds from #{REMOTE_URL}"
+  end
+
   def rewrite_env(env)
-    if USE == :remote
+    if USE == 'remote'
       env["HTTP_HOST"] = REMOTE_URL
     else
       env["HTTP_HOST"] = LOCAL_URL
